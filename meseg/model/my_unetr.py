cd22fc3c85d2d8526dd5a8a780dee22309838331
Donghyun Kim <dk58319@ajou.ac.kr>
2023-02-08 13:47:49 +0900

    Experiment 02/08 _ adopt Refine Transformer

--- meseg/model/my_unetr.py
+++ meseg/model/my_unetr.py
@@ -4,19 +4,105 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+
+class Conv2dSamePadding(nn.Conv2d):
+    """ 2D Convolutions like TensorFlow """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, groups=1, bias=True):
+        super().__init__(in_channels, out_channels, kernel_size, stride, 0, dilation, groups, bias)
+        self.stride = self.stride if len(self.stride) == 2 else [self.stride[0]] * 2
+
+    def forward(self, x):
+        ih, iw = x.size()[-2:]
+        kh, kw = self.weight.size()[-2:]
+        sh, sw = self.stride
+        oh, ow = math.ceil(ih / sh), math.ceil(iw / sw)
+        pad_h = max((oh - 1) * self.stride[0] + (kh - 1) * self.dilation[0] + 1 - ih, 0)
+        pad_w = max((ow - 1) * self.stride[1] + (kw - 1) * self.dilation[1] + 1 - iw, 0)
+        if pad_h > 0 or pad_w > 0:
+            x = F.pad(x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2])
+        return F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+class DLA(nn.Module):
+    def __init__(self, inp, oup, kernel_size=3, stride=1, expand_ratio=3, refine_mode='none'):
+        super(DLA, self).__init__()
+        """
+            Distributed Local Attention used for refining the attention map.
+        """
+
+        hidden_dim = round(inp * expand_ratio)
+        self.expand_ratio = expand_ratio
+        self.identity = stride == 1 and inp == oup
+        self.inp, self.oup = inp, oup
+        self.high_dim_id = False
+        self.refine_mode = refine_mode
+
+        if refine_mode == 'conv':
+            self.conv = Conv2dSamePadding(hidden_dim, hidden_dim, (kernel_size, kernel_size), stride, (1, 1), groups=1,
+                                          bias=False)
+        elif refine_mode == 'conv_exapnd':
+            if self.expand_ratio != 1:
+                self.conv_exp = Conv2dSamePadding(inp, hidden_dim, 1, 1, bias=False)
+                self.bn1 = nn.BatchNorm2d(hidden_dim)
+            self.depth_sep_conv = Conv2dSamePadding(hidden_dim, hidden_dim, (kernel_size, kernel_size), stride, (1, 1),
+                                                    groups=hidden_dim, bias=False)
+            self.bn2 = nn.BatchNorm2d(hidden_dim)
+
+            self.conv_pro = Conv2dSamePadding(hidden_dim, oup, 1, 1, bias=False)
+            self.bn3 = nn.BatchNorm2d(oup)
+
+            self.relu = nn.ReLU6(inplace=True)
+
+    def forward(self, input):
+        x = input
+        if self.refine_mode == 'conv':
+            return self.conv(x)
+        else:
+            if self.expand_ratio != 1:
+                x = self.relu(self.bn1(self.conv_exp(x)))
+            x = self.relu(self.bn2(self.depth_sep_conv(x)))
+            x = self.bn3(self.conv_pro(x))
+            if self.identity:
+                return x + input
+            else:
+                return x
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1, 1)
+        return x * y
+
+
 class PatchEmbed(nn.Module):
     def __init__(self,input_dim,embed_dim,patch_size):
         super().__init__()
         self.conv1 = nn.Conv3d(in_channels=input_dim, out_channels=embed_dim, kernel_size=patch_size, stride=patch_size)
         self.norm = nn.BatchNorm3d(embed_dim)
+        self.se = SELayer(embed_dim)
         self.conv2  = nn.Conv3d(embed_dim, embed_dim, kernel_size=1, stride=1)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
+        v = x
         x = self.conv1(x)
         x = self.relu(x)
         x = self.norm(x)
+       # x = self.se(x)
         x = self.conv2(x)
+        x = self.relu(x)
+        x = self.norm(x)
 
         return x
 
@@ -185,14 +271,15 @@ class TransformerBlock(nn.Module):
         self.mlp_dim = int((cube_size[0] * cube_size[1] * cube_size[2]) / (patch_size * patch_size * patch_size))
         self.mlp = PositionwiseFeedForward(embed_dim, 2048)
         self.attn = SelfAttention(num_heads, embed_dim, dropout, mul_head= mul_head)
+        self.attn_refine = Refined_Attention(dim= embed_dim,num_heads=num_heads,attn_drop= dropout)
 
     def forward(self, x):
         h = x
         x = self.attention_norm(x)
-        x, weights = self.attn(x)
+        #x, weights = self.attn(x)
+        x,weights= self.attn_refine(x)
         x = x + h
         h = x
-
         x = self.mlp_norm(x)
         x = self.mlp(x)
 
@@ -223,6 +310,65 @@ class Transformer(nn.Module):
         return extract_layers
 
 
+class Refined_Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., expansion_ratio=3,
+                 share_atten=False, apply_transform=True, refine_mode='conv_exapnd', kernel_size=3, head_expand=1):
+        """
+            refine_mode: "conv" represents only convolution is used for refining the attention map;
+                         "conv-expand" represents expansion and conv are used together for refining the attention map;
+            share_atten: If set True, the attention map is not generated; use the attention map generated from the previous block
+        """
+        super().__init__()
+        self.num_heads = num_heads
+        self.share_atten = share_atten
+        head_dim = dim // num_heads
+        self.apply_transform = apply_transform
+
+        self.scale = qk_scale or head_dim ** -0.5
+
+        if self.share_atten:
+            self.DLA = DLA(self.num_heads, self.num_heads, refine_mode=refine_mode)
+            self.adapt_bn = nn.BatchNorm2d(self.num_heads)
+
+            self.qkv = nn.Linear(dim, dim, bias=qkv_bias)
+        elif apply_transform:
+            self.DLA = DLA(self.num_heads, self.num_heads, kernel_size=kernel_size, refine_mode=refine_mode,
+                           expand_ratio=head_expand)
+            self.adapt_bn = nn.BatchNorm2d(self.num_heads)
+            self.qkv = nn.Linear(dim, dim * expansion_ratio, bias=qkv_bias)
+        else:
+            self.qkv = nn.Linear(dim, dim * expansion_ratio, bias=qkv_bias)
+
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x, atten=None):
+        B, N, C = x.shape
+        if self.share_atten:
+            attn = atten
+            attn = self.adapt_bn(self.DLA(attn)) * self.scale
+
+            v = self.qkv(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+            attn_next = atten
+        else:
+            qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+            q, k, v = qkv[0], qkv[1], qkv[2]
+
+            q = math.sqrt(self.scale) * q
+            k = math.sqrt(self.scale) * k
+            attn = (q @ k.transpose(-2, -1))  # * self.scale
+            attn = attn.softmax(dim=-1) + atten * self.scale if atten is not None else attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            if self.apply_transform:
+                attn = self.adapt_bn(self.DLA(attn))
+            attn_next = attn
+        x = (attn @ v).transpose(1, 2).reshape(B, attn.shape[-1], C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x, attn_next
+
+
 class UNETR2(nn.Module):
     def __init__(self, img_shape=(128, 128, 128), input_dim=4, output_dim=3, embed_dim=768, patch_size=16, num_heads=12, dropout=0.1,mul_head=1):
         super().__init__()
@@ -237,6 +383,8 @@ class UNETR2(nn.Module):
         self.ext_layers = [3, 6, 9, 12]
 
         self.patch_dim = [int(x / patch_size) for x in img_shape]
+
+
 
         # Transformer Encoder
         self.transformer = \
@@ -334,5 +482,4 @@ if __name__ == '__main__':
     model = UNETR2((96,96,96),1,14,mul_head=3)
     y = model(x)
     print(f"input of model shape:{x.shape}") 
-    print(f"output of model shape:{y.shape}") 
-    
+    print(f"output of model shape:{y.shape}")
--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 # UNETR
-python train.py -s btcv -m unetr \
+python train.py -s btcv -m dh_unet \
 --print-freq 100 --valid-freq 500 --max-iter 25000 \
--proj donghyunkim --use-wandb --cuda 1   # -proj의 이름과 cuda만 바꾸싶쇼
+-proj donghyunkim --use-wandb --cuda 3 mul_head 1   # -proj의 이름과 cuda만 바꾸싶쇼
 
 
 
